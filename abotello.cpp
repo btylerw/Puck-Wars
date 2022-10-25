9053bde3bbaface21494bebf76ef3ef17539c853
Student <student@lab-240-015.cs.csubak.edu>
2022-10-25 07:34:49 -0700

    added pause feature

--- abotello.cpp
+++ abotello.cpp
@@ -1,8 +1,23 @@
 // Andres Botello
 // sourcefile
+#include <GL/glx.h>
+#include "abotello.h"
+#include <iostream>
+//pause state
 unsigned int manage_pstate(unsigned int s)
 {
         s = s ^ 1;
         return s;
 }
-
+extern void pause_screen(GLuint pauseTexture, int xres, int yres)
+{
+        glBindTexture(GL_TEXTURE_2D, pauseTexture);
+        glColor3f(1.0f, 1.0f, 1.0f);
+        glBegin(GL_QUADS);
+            glTexCoord2f(0.0f, 1.0f);   glVertex2i(0,   0);
+            glTexCoord2f(0.0f, 0.25f);  glVertex2i(0,   yres);
+            glTexCoord2f(1.0f, 0.25f);  glVertex2i(xres, yres);
+            glTexCoord2f(1.0f, 1.0f);   glVertex2i(xres, 0);
+        glEnd();
+        glBindTexture(GL_TEXTURE_2D, 0);
+}
--- abotello.h
+++ abotello.h
@@ -1,2 +1,3 @@
+//Header file for Andres Botello
 extern unsigned int manage_pstate(unsigned int s);
-
+extern void pause_screen(GLuint pauseTexture, int xres, int yres);
