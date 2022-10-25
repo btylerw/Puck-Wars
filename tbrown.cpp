948c9bad1bca98499ae99af8999ddd4b779975b9
btylerw <107960406+btylerw@users.noreply.github.com>
2022-10-25 09:45:12 -0700

    Added random placed bricks (F12 to activate)

--- tbrown.cpp
+++ tbrown.cpp
@@ -3,6 +3,7 @@
 #include <GL/glx.h>
 #include "tbrown.h"
 #include <iostream>
+#include <cstdlib>
 
 // Renders intro screen
 extern void show_intro_screen(GLuint introTexture, int xres, int yres)
@@ -29,4 +30,29 @@ extern void show_background(GLuint backgroundTexture, int xres, int yres)
             glTexCoord2f(1.0f, 1.0f);   glVertex2i(xres, 0);
         glEnd();
         glBindTexture(GL_TEXTURE_2D, 0);
+}
+
+extern int change_brick_vel(int i, float pos)
+{
+	srand(time(NULL));
+	int selection = rand() % 19 + 1;
+
+	if (i < 10 && i == selection) {
+		return 10;
+	}
+	else if (i >= 10 && i == selection) {
+		return -10;
+	}
+	else
+		return 0;
+}
+
+extern int reset_brick_pos(int i, float w, int xres)
+{
+    if (i < 10) {
+		return -w;
+    }
+
+    else
+		return w + xres;
 }
--- tbrown.h
+++ tbrown.h
@@ -1,4 +1,7 @@
 // Header file for Tyler Brown
 
+
 extern void show_intro_screen(GLuint introTexture, int xres, int yres);
 extern void show_background(GLuint backgroundTexture, int xres, int yres);
+extern int change_brick_vel(int i, float pos);
+extern int reset_brick_pos(int i, float w, int xres);
